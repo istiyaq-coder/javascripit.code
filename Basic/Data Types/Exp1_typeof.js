f70a91bff13f7a206a505449652a3faafef31bdb
istiyaq-coder <khanistiyaq8624@gmail.com>
2026-02-15 17:46:37 +0530

    verify typeof of primitive datatype

---
+++ Basic/Data Types/Exp1_typeof.js
@@ -0,0 +1,19 @@
+/*Create variables of all primitive types and log their typeof. */
+
+let a = "khan";
+let b = 12;
+let c = true;
+let d = null;// gives object as a type
+let e = undefined;
+let f = 5423756359n;
+let h = Symbol(5);
+
+console.log( typeof a);
+console.log( typeof b);
+console.log( typeof c);
+console.log( typeof d);
+console.log( typeof e);
+console.log( typeof f);
+console.log( typeof h);
+
+
