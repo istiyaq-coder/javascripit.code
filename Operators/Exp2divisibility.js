c23c8a9baf8da51982b3e614cf43a743698766f5
istiyaq-coder <khanistiyaq8624@gmail.com>
2026-02-16 22:11:29 +0530

    operation quick

---
+++ Operators/Exp2divisibility.js
@@ -0,0 +1,37 @@
+/* Write a program that checks whether a number is divisible by both 3 and 5. */
+
+
+let value = 12
+
+if(value % 3 == 0 && value % 5 == 0)
+
+    {console.log("divisible by both 3 and 5");
+} else{
+    console.log("not divisible by both 3 and 5")};
+
+//             or
+
+
+
+(value % 15 === 0)
+    ? console.log("Divisible by both 3 and 5")
+    : console.log("Not divisible by both 3 and 5");
+
+//               or
+    
+
+(value % 15 === 0)
+    ? console.log("Divisible by both 3 and 5")
+    : console.log("Not divisible by both 3 and 5");
+
+
+
+
+
+
+
+
+
+
+
+
