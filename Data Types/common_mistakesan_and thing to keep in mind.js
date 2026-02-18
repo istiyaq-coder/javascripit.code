44c2cd67949bf04a566d1b2ced01832f9a197907
istiyaq-coder <khanistiyaq8624@gmail.com>
2026-02-18 20:12:41 +0530

    common sense to code with js

---
+++ Data Types/common_mistakesan_and thing to keep in mind.js
@@ -0,0 +1,30 @@
+/* Compare "5" and 5 using == and ===. */
+
+console.log("5" == 5);
+console.log("5" === 5);
+
+/* Write code to convert:
+
+"50" → number
+100 → string */
+
+console.log("50" + 1);
+console.log(100 + "1");
+console.log(typeof ("50" - 1));
+console.log(typeof (100 + "1"));
+//           or
+
+let num = Number("50");
+console.log(num);           // 50
+console.log(typeof num);    // number
+
+let str = String(100);
+console.log(str);           // "100"
+console.log(typeof str);    // string
+
+
+/* coinataion */
+console.log(true + 1);
+console.log(false + 1);
+console.log("10" + 1);
+console.log("10" - 1);
