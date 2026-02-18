dca722595cfe212ca65547b206933793ed358f13
istiyaq-coder <khanistiyaq8624@gmail.com>
2026-02-18 19:59:10 +0530

    check datatype of primartive

---
+++ Data Types/Create variables of differnt type.js
@@ -0,0 +1,20 @@
+/* Create variables of type:
+
+string
+number
+boolean
+undefined
+null
+also cheeck the datatype of it */
+
+let a = "naman jain"; // string
+let b = 67; // number
+let c = true; //boolean
+let d; // undefined
+let e = null; //null
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
