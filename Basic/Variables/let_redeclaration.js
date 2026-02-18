fdf28e2e9d17be3335eb4c5efa6445faecc8e82a
istiyaq-coder <khanistiyaq8624@gmail.com>
2026-02-18 19:16:36 +0530

    redeclation

--- Basic/Variables/let_redeclaration.js
+++ Basic/Variables/let_redeclaration.js
@@ -4,3 +4,26 @@ let a = 10;
 //let a = 20;   redeclaring in same scope is not allowed reference error
 
 console.log(a);
+
+
+/* Create a const variable and try changing it. */
+const b = 10;
+
+// b = 20;    trying to change it
+
+console.log(b);
+
+/* If const holds an object or array:
+
+You cannot reassign, but you can modify inside it.
+
+Example:
+const arr = [1, 2, 3];
+
+arr.push(4);   //allowed
+
+console.log(arr);  // [1, 2, 3, 4]
+
+arr = [5, 6];  // Error
+
+ */
