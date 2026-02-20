1761f1a9742a82f92f70a63b475213775349dff0
istiyaq-coder <khanistiyaq8624@gmail.com>
2026-02-20 23:24:47 +0530

    three methods to solve this problem

--- Loops/Find the sum of all numbers from 1 to 100..js
+++ Loops/Find the sum of all numbers from 1 to 100..js
@@ -1,8 +1,34 @@
 /* Find the sum of all numbers from 1 to 100. */
 let sum = 0;
 
-for (let i = 1; i < 101; i++){
+for (let i = 1; i < 101; i++) {
   sum += i;
 }
 
 console.log(sum);
+
+
+
+//                                  OR
+
+
+
+let n = 100;
+let sumi = (n * (n + 1)) / 2;
+
+console.log(sumi);
+
+
+
+
+ //                               OR
+
+
+let sumin = 0;
+let i = 0;
+
+while (i < 101) {
+    sumin += i;
+  i++;
+}
+console.log(sumin);
