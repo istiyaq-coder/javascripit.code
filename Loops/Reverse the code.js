2821d4e431164ed29f1c26becb15132879296f0c
istiyaq-coder <khanistiyaq8624@gmail.com>
2026-02-21 14:24:21 +0530

    did you understand

--- Loops/Reverse the code.js
+++ Loops/Reverse the code.js
@@ -5,7 +5,13 @@ Example:
 Input: 1234
 Output: 4321 */
 
-let num = 4;
-for (let i = num; i >= 1 ; i--) {
-  console.log(i);
+let num = 1234507980;
+let reversed = 0;
+
+while (num > 0) {
+    let digit = num % 10;        // get last digit
+    reversed = reversed * 10 + digit; 
+    num = Math.floor(num / 10);  // remove last digit
 }
+
+console.log(reversed);
